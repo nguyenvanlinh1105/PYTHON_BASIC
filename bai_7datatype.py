029fc3d33b59614c462d4a9ac21137bf341db704
Linh Nguyen <vanlinh11052004nguyenvan@gmail.com>
2024-01-05 01:17:55 +0700

    upload new file

---
+++ bai_7datatype.py
@@ -0,0 +1,18 @@
+# KIỂU DỮ LIỆU CƠ BẢN TRONG PYTHON 
+# python là một ngôn ngữ thông dịch : không cần biên dịch ra mã máy mà thực thi luôn 
+
+"""
+KIỂU DỮ LIỆU            VÍ DỤ               MÔ TẢ
+int                     x = 1               số nguyên 
+
+float                   y = 3.4             số thực 
+
+complex                 x = 1 +3j           số phức 
+
+bool                    x = True            Boolean: T/F
+
+str                     x ='abc'            String : chuỗi kí tự 
+
+Nonetype                x='None'            Đối tượng đặc biệt chỉ ra giá trị null 
+
+"""
