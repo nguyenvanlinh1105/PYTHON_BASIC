bdcd7b74510be628731a3528151c3470d4ace16a
Linh Nguyen <vanlinh11052004nguyenvan@gmail.com>
2024-01-07 23:09:58 +0700

    upload new file

--- bai_24tuple.py
+++ bai_24tuple.py
@@ -1,3 +1,29 @@
 """
 Tuple : là một chuỗi các phần tử có thử tự như một list. Sự khác biệt duy nhất là bộ giá trị là các hằng số 
+- Đặc biệt của tuple là khi tạo ra thì giá trị của nó không thể sửa đổi.
+- tuple được sử dụng để bảo vệ dữ liệu và thường nhanh hơn danh sách vì chúng không thể thay đổi động 
+- Được định nghĩa trong dấu ngoặc đơn(), các mục được phân tách bằng dấu phẩy.
+- giá trị của tuple có thể bị trùng lặp 
+
+
 """
+
+TraiCay=('Na','Lựu','Hồng','Cam','Quýt')
+so=(1,2,3,4,5,6)
+print(TraiCay)
+print(so)
+
+# Các thao tác với Tuple
+# lấy phần tử 
+print(so[0])
+print(so[1:4])
+# Cộng 2 tuple 
+y = (1,2, 3, 4, 7,9)
+z =('a','g', 'b')
+t= y+z
+print(t)
+# phép nhân 
+k = y*2
+print(k)
+
+
---
+++ bai_25set.py
@@ -0,0 +1,23 @@
+"""
+Set là một trong 4 kiểu dữ liệu tích hợp sẵn trong Python dùng để lưu trữ các tập hợp là dữ liệu, 3 kiểu còn lại là list, Tuple và Dictionary, tất cả đều có chất lượng và cách sử dụng khác nhau 
+Set là tập hợp không có thứ tự, không thể thay đổi và không được lập chỉ mục. Lưu ý : Các mục set là không thể thay đổi nhưng bạn có thể xóa và thêm các mục mới 
+ Sử dụng cặp ngoặc { }
+"""
+
+Set ={'Gà','Vịt','Heo','Chó'}
+print(Set)
+
+for x in Set:
+    print(x)
+
+# xóa 
+Set.remove("Gà")
+print(Set)
+
+# Thêm 
+Set.
+print(Set)
+
+
+
+
