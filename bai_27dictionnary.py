144a45b2aa38b128f43ac9225bda93760ecab186
Linh Nguyen <vanlinh11052004nguyenvan@gmail.com>
2024-01-08 14:39:33 +0700

    update file bai_17.py

--- bai_27dictionnary.py
+++ bai_27dictionnary.py
@@ -7,3 +7,55 @@ Dictionary: Từ điển được sử dụng để lưu trữ các giá trị d
 - Từ điển có thể thay đổi, nghĩa là chúng có thể thay đổi, thêm hoặc bớt các mục sau khi từ điển đã được tạo 
 """
 
+Sinhvien={
+    "HoVaTen":"Nguyễn Văn Linh",
+    "maLop":"22T2"
+}
+Sinhvien1={
+    "HoVaTen":"Nguyễn Văn Hào",
+    "maLop":"22T3"
+}
+list =[Sinhvien,Sinhvien1]
+tuple =(Sinhvien)
+print(Sinhvien)
+# lấy dữ liệu
+print(Sinhvien["HoVaTen"])
+# cách 2: 
+print(Sinhvien.get("maLop"))
+
+# thay đổi giá trị 
+Sinhvien["maLop"]="DH02"
+Sinhvien.update({"linh nguyen":"deptry"}) # có thể thêm mới hoặc thêm giá trị 
+
+# thêm mục mới 
+Sinhvien["namHoc"]=2024
+Sinhvien["noiSinh"]="Giolinh"
+
+# xóa đi các mục 
+Sinhvien.pop("noiSinh")  # loại bỏ mục có tên khóa được chỉ định
+
+# popitem() xóa mục được chèn cuối cùng 
+Sinhvien.popitem()
+
+# xóa luôn 
+del Sinhvien["HoVaTen"]
+
+# làm trống dictionnary
+Sinhvien.clear()
+
+# duyệt các giá trị 
+for x in Sinhvien1.values():
+    print(x)
+# duyet key
+for x in Sinhvien1.keys():
+    print(x)
+# duyệt các cặp khóa -item
+for x, y in Sinhvien1.items():
+    print(x,y)
+
+
+print(list)
+print(tuple)
+
+
+
